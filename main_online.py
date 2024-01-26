4620ba81bb4a1ce67905483d4f94fec515f44a9b
Samuel Paluba <samuel.paluba@gmail.com>
2024-01-26 18:29:50 +0100

    modified: main_online.py

--- main_online.py
+++ main_online.py
@@ -1,16 +1,31 @@
 import requests
 import json
 import datetime
+import requests
+import json
+import datetime
 
 
-def weather_for_city(city, api):
+"""def weather_for_city(city, api):
   url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api
   response = requests.get(url)
-  print (response.content)
-  """
-  data = json.loads(response.content)
-  # raw data
-  """
+ 
+  print(response.content) """
+def weather_for_city(city, api, file, format="utf-8"):
+  url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api
+  response = requests.get(url)
+  #data_format = json.dump(data, indent=2)
+  
+  # Save response.content to a text file
+  with open(file, 'w') as file:
+    file.write(response.content.decode(format))
+    
+    data = json.loads(response.content)
+
+
+
+
+  
   weather_raw_tempature = data['main']['temp']
   weather_raw_feel_like = data['main']['feels_like']
   weather_raw_pressure = data['main']['pressure']
@@ -22,30 +37,44 @@ def weather_for_city(city, api):
   weather_raw_wind_deg = data['wind']['deg']
   weather_raw_wind_gust = data['wind']['gust']
   weather_raw_clouds = data['clouds']['all']
-  # full data
+  
+  # Generate weather_full_* variables
+  weather_full_tempature = 'Temperature: {} degrees Celsius'.format(weather_raw_tempature)
+  weather_full_feel_like = 'Feels Like: {} degrees Celsius'.format(weather_raw_feel_like)
+  weather_full_pressure = 'Pressure: {} hPa'.format(weather_raw_pressure)
+  weather_full_tempature_min = 'Min Temperature: {} degrees Celsius'.format(weather_raw_tempature_min)
+  weather_full_tempature_max = 'Max Temperature: {} degrees Celsius'.format(weather_raw_tempature_max)
+  weather_full_humidity = 'Humidity: {} %'.format(weather_raw_humidity)
+  weather_full_wind_speed = 'Wind Speed: {} m/s'.format(weather_raw_wind_speed)
+  weather_full_sky_condition = 'Sky Condition: {}'.format(weather_raw_sky_condition)
+  weather_full_wind_deg = 'Wind Degree: {}'.format(weather_raw_wind_deg)
+  weather_full_wind_gust = 'Wind Gust: {}'.format(weather_raw_wind_gust)
+  weather_full_clouds = 'Clouds: {} %'.format(weather_raw_clouds)
+  
+  # Print weather_full_* variables
+  print(weather_full_tempature)
+  print(weather_full_feel_like)
+  print(weather_full_pressure)
+  print(weather_full_tempature_min)
+  print(weather_full_tempature_max)
+  print(weather_full_humidity)
+  print(weather_full_wind_speed)
+  print(weather_full_sky_condition)
+  print(weather_full_wind_deg)
+  print(weather_full_wind_gust)
+  print(weather_full_clouds)
 
+
+  
+"""
   weather_full_city = '**Weather for {}**'.format(data['name'])
   weather_full_tempature = 'Temperature: {} degrees Celsius'.format(data['main']['temp'])
   weather_full_humidity = 'Humidity: {} %'.format(data['main']['humidity'])
   weather_full_wind_speed = 'Wind speed: {} m/s'.format(data['wind']['speed'])
   weather_full_sky_condition = 'Sky condition: {}'.format(data['weather'][0]['description'])
-
-  print(weather_raw_tempature)
-  print(weather_raw_feel_like)
-  print(weather_raw_pressure)
-  print(weather_raw_tempature_min)
-  print(weather_raw_tempature_max)
-  print(weather_raw_humidity)
-  print(weather_raw_wind_speed)
-  print(weather_raw_sky_condition)
-  print(weather_raw_wind_deg)
-  print(weather_raw_wind_gust)
-  print(weather_raw_clouds)
-  
+"""
 
 
 
 
-
-
-weather_for_city("Berlin","4e4e25da3c76c95c7c348925a7e2310a")
+weather_for_city("Berlin","4e4e25da3c76c95c7c348925a7e2310a", "json.json")
--- tmp.py
+++ tmp.py
@@ -1,8 +1,107 @@
-"""
-  print('**Weather for {}**'.format(data['name']))
-  print('Temperature:', data['main']['temp'], 'degrees Celsius')
-  print('Humidity:', data['main']['humidity'], '%')
-  print('Wind speed:', data['wind']['speed'], 'm/s')
-  print('Sky condition:', data['weather'][0]['description'])
-"""
-weather.locate("json.json")
+import turtle
+import time
+import random
+
+delay = 0.1
+
+# Set up the screen
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0) 
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+segments = []
+
+# Functions
+def go_up():
+    head.direction = "up"
+
+def go_down():
+    head.direction = "down"
+
+def go_left():
+    head.direction = "left"
+
+def go_right():
+    head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Keyboard bindings
+wn.listen()
+wn.onkey(go_up, "w")
+wn.onkey(go_down, "s")
+wn.onkey(go_left, "a")
+wn.onkey(go_right, "d")
+
+# Main game loop
+while True:
+    wn.update()
+
+    # Check for collision with border
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+        
+    # Check for collision with food
+    if head.distance(food) < 20:
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
+
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+    # Move the segments in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to head
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+        
+    move()     
+    time.sleep(delay)
